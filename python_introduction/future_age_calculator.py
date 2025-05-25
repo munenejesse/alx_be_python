95474001797d0bbdc3a1b37702c3733f2862bcbe
Jesse <Jessymunene9@gmail.com>
2025-05-25 15:42:06 +0300

    commit first changes to future age calc

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,5 @@
+current_age = int(input("How old are you? "))
+#age ij 2050
+age = current_age + 27
+ # print result
+print(f"In 2050, you will be {age} ")
--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -3,5 +3,6 @@ length = 10
 width = 5
 #calculate the area of a rectangle
 area = length * width
- #print the area
+
+# Print the result
 print(f"The area of the rectangle is: {area}")
