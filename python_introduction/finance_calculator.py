43bc6c8597995847e9b1a9671eea7628575031ce
Jesse <Jessymunene9@gmail.com>
2025-05-25 16:11:46 +0300

    changed imout values to int for finance calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,11 +1,10 @@
 #Prompt the user for their monthly income
-monthlyIncome = int(input("Enter your monthly income: "))
+monthly_income = int(input("Enter your monthly income: "))
 #Ask for their total monthly expenses:
-totalMonthlyExpense = int(input("Enter your total monthly expenses: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
 #Calculate the monthly savings
-montlySavings = monthlyIncome - totalMonthlyExpense
+monthly_savings = monthly_income - monthly_expenses
 #Project Annual Savings:
-ProjectedAnnualSavings = ((montlySavings*12) * 5/100 *1) + (montlySavings*12)
-
-print(f"Your monthly savings are {montlySavings} ")
+ProjectedAnnualSavings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+print(f"Your monthly savings are {monthly_savings}")
 print(f"Projected savings after one year, with interest, is: {ProjectedAnnualSavings}")
