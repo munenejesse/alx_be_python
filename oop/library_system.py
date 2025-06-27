bd7357ff550a11f09f0af64fce2c36726bed2d3a
Jesse <Jessymunene9@gmail.com>
2025-06-27 22:26:10 +0300

    Initial commit - OOP library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,7 +3,7 @@ class Book:
         self.title = title
         self.author = author
 
-    def get_details(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
 
@@ -12,7 +12,7 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size
 
-    def get_details(self):
+    def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
@@ -21,7 +21,7 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def get_details(self):
+    def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
@@ -30,8 +30,11 @@ class Library:
         self.books = []
 
     def add_book(self, book):
-        self.books.append(book)
+        if isinstance(book, Book):
+            self.books.append(book)
+        else:
+            print("Only Book instances can be added to the library.")
 
     def list_books(self):
         for book in self.books:
-            print(book.get_details())
+            print(book)
--- oop/main.py
+++ oop/main.py
@@ -1,5 +1,4 @@
 from book_class import Book
-from library_system import Book, EBook, PrintBook, Library
 
 def main():
     # Creating an instance of Book
@@ -14,26 +13,6 @@ def main():
     # Deleting a book instance to trigger __del__
     del my_book
 
-    # Create a Library instance
-    my_library = Library()
-    # Create instances of each type of book
-    classic_book = Book("Pride and Prejudice", "Jane Austen")
-    digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
-    paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
-
-    # Add books to the library
-    my_library.add_book(classic_book)
-    my_library.add_book(digital_novel)
-    my_library.add_book(paper_novel)
-
-    # List all books in the library
-    my_library.list_books()
-
-
 if __name__ == "__main__":
     main()
-
-
-
-
 
